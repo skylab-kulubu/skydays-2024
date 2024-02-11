67cadb5c7f05a9762f16dc153ff7f72c6e0d954c
GokdenizCakir <gokdenizckr@gmail.com>
2024-02-11 17:09:55 +0300

    made landing and about page responsive

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section
       id='1'
-      className='h-[100dvh] snap-start px-4 sm:px-10 md:px-24 lg:px-[10rem] pt-32 sm:pt-64'
+      className='h-[100dvh] flex flex-col justify-center sm:block snap-start px-4 sm:px-10 md:px-24 lg:px-[10rem] pt-32 sm:pt-52'
     >
       <div
         id='nav-gradient'
@@ -13,16 +13,16 @@ const About = () => {
         <h2 className='text-3xl sm:text-6xl lg:text-[5.5rem] font-bold'>
           Hakkımızda
         </h2>
-        <p className='text-lg sm:text-2xl md:text-3xl lg:text-4xl font-extralight'>
+        <p className='text-lg sm:text-2xl font-extralight'>
           Bu yıl 5. kez düzenlenecek olan SKYDAYS, 22-23 Mart tarihlerinde
           gerçekleşecek bir siber güvenlik etkinliğidir. Etkinlik 2 gün sürecek
           olup seminer ve CTF’ten oluşacaktır
           <br />
           <br />
-          Etkinliğimizin ilk gününde katılımcılarımız;birbirinden değerli
-          isimlerin bu alandakisunumlarını dinleyecek, fuaye alanlarıylanetwork
-          elde edecek ve çeşitli yarışmalar ilede hediyeler kazanabilecek.
-          Etkinlik ikincigününde ise CTF yarışması ile sonlanacaktır.
+          Etkinliğimizin ilk gününde katılımcılarımız; birbirinden değerli
+          isimlerin bu alandaki sunumlarını dinleyecek, fuaye alanlarıyla network
+          elde edecek ve çeşitli yarışmalar ile de hediyeler kazanabilecek.
+          Etkinliğin ikinci gününde ise CTF yarışması ile sonlanacaktır.
         </p>
       </div>
     </section>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,27 +5,31 @@ const Landing = () => {
   return (
     <section
       id='0'
-      className='snap-start pt-72 h-[100dvh] flex justify-between'
+      className='snap-start pt-32 md:pt-72 h-[100dvh] flex flex-col md:flex-row items-center justify-center gap-4 sm:gap-12 md:gap-0 md:justify-between'
     >
       <div>
-        <div className='pl-[5.5rem]'>
-          <h2 className='text-3xl tracking-wider'>
+        <div className='pl-4 sm:pl-8 lg:pl-[5.5rem]'>
+          <h2 className='text-lg sm:text-2xl lg:text-3xl tracking-wider'>
             SKY LAB KULÜBÜ'nden <br />
-            <span className='text-[6rem] font-extrabold leading-[5.5rem]'>
-              5.SKYDAYS :
+            <span className='text-6xl sm:text-7xl lg:text-[6rem] font-extrabold sm:leading-[5.5rem]'>
+              5.SKYDAYS:
             </span>
             <br />
             Türkiye’nin En büyük siber güvenlik etkinliği
           </h2>
         </div>
 
-        <div className='mt-12 text-4xl font-extralight py-4 bg-gradient-to-r from-[#035A81] to-transparent px-[5.5rem]'>
+        <div className='mt-4 sm:mt-12 text-2xl sm:text-4xl font-extralight py-4 bg-gradient-to-r from-[#035A81] to-transparent px-4 sm:px-8 lg:px-[5.5rem]'>
           <h2>
             22-23 Mart 2024 <br /> YTU Davutpaşa tarihi Hamam
           </h2>
         </div>
       </div>
-      <img className='h-96 mr-[5.5rem]' src={img1} alt='img1' />
+      <img
+        className='h-64 md:h-80 mr-89 lg:mr-[5.5rem]'
+        src={img1}
+        alt='img1'
+      />
     </section>
   );
 };
