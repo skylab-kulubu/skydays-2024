533093ecfef4a6f42be0d2dd0cd097cdb6f1898b
GokdenizCakir <gokdenizckr@gmail.com>
2024-02-11 15:37:39 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   }, [currentSection]);
 
   return (
-    <div className='App h-[100dvh] relative snap-mandatory snap-y overflow-y-scroll overflow-x-hidden'>
+    <div className='App h-[100dvh] relative snap-proximity snap-y overflow-y-scroll overflow-x-hidden'>
       <div
         style={{ backgroundImage: `url('${background}')` }}
         className='absolute h-[580dvh] -z-10 w-screen bg-cover '
